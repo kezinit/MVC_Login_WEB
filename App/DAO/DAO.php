bef1f1bd8f54d8b0c62f4e77faf56e3dc4da9145
Your Name <you@example.com>
2022-10-18 11:18:12 -0300

    adicionando o projeto ao github

---
+++ App/DAO/DAO.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DAO;
+
+use \PDO;
+
+abstract class DAO 
+
+{
+    protected $conexao;
+    public function __construct()
+    {
+        // DSN (Data Source Name) onde o servidor MySQL será encontrado
+        // (host) em qual porta o MySQL está operado e qual o nome do banco pretendido
+        // Mais informações sobre DSN: https://www.php.net/manual/pt_BR/ref.pdo-mysql.connection.php
+        $dsn = "mysql:host=" . $_ENV['db']['host'] . ";dbname=" . $_ENV['db']['database'];
+
+        // Criando a conexão e armazenado na propriedade definida para tal.
+        // Veja o que é PDO: https://www.php.net/manual/pt_BR/intro.pdo.php
+         $this->conexao = new PDO($dsn, $_ENV['db']['user'], $_ENV['db']['pass']);
+    }
+}
---
+++ App/rotas.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\CategoriaProdutoController;
+use App\Controller\PessoaController;
+use App\Controller\ProdutoController;
+
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+    
+
+
+switch($url)
+{
+    case '/login':
+        LoginController::index();
+    break;
+
+    case '/login/auth':
+        LoginController::auth();
+    break;
+
+    case '/logout':
+        LoginController::logout();
+    break;
+
+
+    case '/':
+        echo "página inicial";
+    break;
+
+
+    case '/pessoa':
+        // Para saber mais sobre o Operador de Resolução de Escopo (::), 
+        // leia: https://www.php.net/manual/pt_BR/language.oop5.paamayim-nekudotayim.php
+        PessoaController::index();
+    break;
+
+
+    case '/pessoa/form':
+        PessoaController::form();
+    break;
+
+    case '/pessoa/form/save':
+        PessoaController::save();
+    break;
+
+
+    case '/pessoa/delete':
+        PessoaController::delete();
+    break;
+
+    /**
+     * Rotas para produto abaixo
+     */
+
+    case '/produto':
+        
+        ProdutoController::index();
+    break;
+
+
+    case '/produto/form':
+        ProdutoController::form();
+    break;
+
+    case '/produto/form/save':
+        ProdutoController::save();
+    break;
+
+    
+    case '/produto/delete':
+        ProdutoController::delete();
+    break;
+
+    /**
+     * Rotas para CategoriaProduto abaixo
+     */
+
+    case '/categoriaproduto':
+        
+        CategoriaProdutoController::index();
+    break;
+
+    case '/categoriaproduto/form':
+        CategoriaProdutoController::form();
+    break;
+
+    case '/categoriaproduto/form/save':
+        CategoriaProdutoController::save();
+    break;
+
+    case '/categoriaproduto/delete':
+        CategoriaProdutoController::delete();
+    break;
+
+    default:
+        echo "Erro 404";
+    break;
+}
